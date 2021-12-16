a96f2c748718766d0be9cb9267e01036c163d33d
ahmadsyaifulakbar24 <ipulbelcram@gmail.com>
2021-12-16 09:59:08 +0700

    add privacy policy

--- src/component/login/ModalPrivacy.jsx
+++ src/component/login/ModalPrivacy.jsx
@@ -1,6 +1,24 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { connect } from 'react-redux'
 
-function ModalPrivacy() {
+function ModalPrivacy(props) {
+  const [privacyPoicy, setPrivacyPolicy] = useState('')
+
+  const getPrivacyPolicy = async () => {
+      await axios.get(props.base_url + 'privacy-policy')
+      .then((res) => {
+          setPrivacyPolicy(res.data.content)
+      }).catch((err) => {
+        console.log(err.response)
+      })
+  }
+
+  useEffect(() => {
+      getPrivacyPolicy()
+  }, [])
+
+
     return (
         <div class="modal fade" id="privacyModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog modal-xl">
@@ -10,101 +28,7 @@ function ModalPrivacy() {
               <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
-            <p>Pembaruan Terakhir: 23 Februari 2021</p>
-            <p>Kebijakan Privasi ini menjelaskan bagaimana PT. Haier Sales Indonesia, anak usahanya dan/atau perusahaan yang terafiliasi (“ <strong>HSI</strong>”, “ <strong>kami</strong>”) memproses dan mengontrol data pribadi anda (“ <strong>Data Pribadi Anda</strong>”) serta juga hak-hak hukum anda yang terkait dengan pengumpulan dan pemrosesan Data Pribadi anda. </p>
-            <p>Kebijakan ini selaras dengan kebijakan internal dan petunjuk pelaksanaan dari Grup Haier sehubungan dengan data pribadi serta juga peraturan perundang-undangan yang berlaku di Indonesia. Kami dapat mengubah Kebijakan Privasi ini dari waktu ke waktu. Versi terbaru dari Kebijakan Privasi akan kami tampilkan di situs web kami dengan mencantumkan tanggal pembaharuan terakhir.</p>
-            <p>
-              <strong>Perlindungan Data Pribadi Anda</strong>
-            </p>
-            <p>Perlindungan Data Pribadi Anda adalah komitmen dan tanggung jawab bernilai tinggi dan sangat penting bagi HSI. Ketika berhubungan dengan HSI, kami ingin anda merasa aman dan yakin bahwa Data Pribadi anda berada di tangan yang tepat. HSI akan mememastikan bahwa:</p>
-            <ol>
-              <li>Akses terhadap Data Pribadi anda terbatas kepada karyawan yang bertugas untuk memproses Data Pribadi anda sesuai dengan pemenuhan kebutuhan pekerjaannya; dan</li>
-              <li>Hanya informasi-informasi yang diperlukan saja yang dikemukakan kepada karyawan</li>
-            </ol>
-            <p>Kami mengumpulkan Data Pribadi secara langsung dari anda. Kami mungkin akan merekam detail data kontak ketika anda menghubungi kami, tim <i>customer service</i> kami, ketika anda mengisi form aplikasi (termasuk form lamaran kerja), ketika anda membeli salah satu produk kami, ketika anda memberikan detail kontak anda melalui situs web, telepon genggam, atau email atau ketika anda bertanya mengenai suatu informasi kepada kami atau meminta sesuatu dari kami. </p>
-            <p>Kami mungkin akan mengumpulkan dan memproses Data Pribadi anda sesuai dengan kategori sebagai berikut:</p>
-            <ol>
-              <li>Data Kontrak dan Informasi Kontrak Bisnis seperti: nama, rincian perusahaan, nomor telepon, kode pos, alamat <i>email</i>, informasi kontak lainnya, termasuk informasi kontak bisnis anda, rekening bank perusahaan, dan data-data lainnya yang dapat memungkinkan kami untuk menjaga hubungan bisnis dengan anda; </li>
-              <li>Data yang ditargetkan: usia, ketertarikan, unggahan, komentar, atau bentuk konten lain nya yang anda unggah atau kirimkan di situs web kami, pergerakan dan kebiasaan;</li>
-              <li>Data yang dikumpulkan secara otomatis: waktu dan durasi kunjungan ke situs web, informasi mengenai alat pencarian ( <i>browser</i>), sistem operasi, dan Data Pribadi lainnya yang relevan dan diperlukan untuk kelancaran pelaksanaan layanan. </li>
-            </ol>
-            <p>Jika anda menyediakan Data Pribadi milik orang lain kepada kami, anda bertanggung jawab penuh untuk memastikan bahwa orang tersebut sepenuhnya mengetahui tentang Informasi yang terdapat dalam Kebijakan Privasi ini dan orang tersebut telah memberikan persetujuannya untuk memberikan informasi tersebut kepada kami.</p>
-            <p>
-              <strong>Tujuan</strong>
-            </p>
-            <p>Kami mungkin akan mengumpulkan dan menggunakan Data Pribadi anda untuk tujuan berikut:</p>
-            <ol>
-              <li>Untuk memenuhi alasan anda menyerahkan informasi. Sebagai contoh, jika anda membagikan nama dan informasi kontak guna meminta suatu layanan atau mengajukan pertanyaan terkait produk atau layanan kami, kami akan mempergunakan Data Pribadi tersebut untuk menjawab pertanyaan anda. Jika anda menyerahkan Data Pribadi anda untuk mengajukan klaim garansi produk atau layanan purna jual, kami akan mepergunakan intormasi tersebut untuk melakukan proses pembayaran dan memfalisitasi pengiriman.</li>
-              <li>Untuk mempersonalisasi pengalaman anda sebagai pengguna situs web dan guna menyampakan konten, produk, dan penawaran layanan yang relevan dengan kebutuhan anda, termasuk iklan-iklan dan penawaran-penawaran yang secara khusus ditargetkan kepada anda melalui situs web kami, situs pihak ketiga, dan melalui email atau pesan singkat (denga persetujuan anda dalam hal dipersyaratkan oleh hukum yang berlaku).</li>
-              <li>Untuk pengetesan, riset, analisis, dan pengembnagan produk, termasuk untuk mengembangkan dan meningkatkan situs web, produk dan layanan kami.</li>
-              <li>Sebagaimana dijelaskan kepada anda pada saat kami mengumpulkan data pribadi anda atau seabgaimana disepaakti antara anda dan kami.</li>
-              <li>Guna berkomunikasi dengan anda terkait produk, layanan, kegiatan, survey, dan promosi (termasuk dengan mengirimkan materi pemasaran).</li>
-              <li>Untuk memproses, mengevaluasi, dan merespon permintaan anda.</li>
-              <li>Untuk memverifikasi identitas diri anda guna memastikan keamanan dari tujuan-tujuan lain sebagaimana tercantum pada daftar ini.</li>
-              <li>Untuk mengoperasikan, mengevaluasi, dan meingkatkan bisnis kami (termasuk mengembangkan produk dan layanan baru, mengatur cara kami berkomunikasi, melaksanakan penelitian pasar, menentukan dan mengatur efektifitas dari aktifitas periklanan dan pemasaran, menganalisa produk, layanan, dan situs web, mengelola situs web kami; dan melaksanakan proses akunting, audit, penagihan, rekonsiliasi, dan penagihan).</li>
-              <li>Untuk menjaga dan menghindari adanya tindakan penipuan, transaksi mencurigakan, klaim, dan tanggung jawab lain nya, serta mengelola kualitas dan tingkat paparan risiko.</li>
-              <li>Untuk menjaga keamanan dari layanan jaringan, sumber daya informasi dan karyawan HSI.</li>
-            </ol>
-            <p>Kami juga mungkin akan mempergunakan data pribadi untuk tujuan-tujuan lainnya, yang akan kami identifikasi pada saat pengumpulan data. Anda berhak untuk tidak memberikan jenis informasi tertentu, namun demikian hal tersebut dapat mempengaruhi kemampuan anda untuk mempergunakan layanan tersebut.</p>
-            <p>
-              <strong>Pengumpulan Data Pribadi</strong>
-              <br />Melalui situs web kami, kami mengumpulkan dan memproses Data Pribadi anda dalam dua cara:
-            </p>
-            <ol>
-              <li>
-                <strong>proses langsung</strong> (sebagai contoh, ketika anda menyerahkan Data Pribadi anda melalui sambungan telepon langsung atau tindakan pada situs web kami atau ketika anda diminta untuk mengisi formulir pada situs web kami atau ketika anda menyerahkan informasi guna mendaftar untuk memperoleh formulir atau mendaftar untuk memberikan komentar pada situs web); dan
-              </li>
-              <li>
-                <strong>proses tidak langsung</strong>: dari pihak ketiga tertentu (sebagai contoh, melalui teknologi analisis situs web). Pihak ketiga tersebut termasuk diantaranya affiliasi kami, otoritas public, situs web public dan media sosial, pemasok dan vendor, dan lain-lain.
-              </li>
-            </ol>
-            <p>
-              <strong>Retensi</strong>
-            </p>
-            <p>Kami akan menyimpan Data Pribadi selama anda masih mempergunakan situs web dan selama kami memiliki hubungan yang sedang berjalan dengan anda untuk jangka waktu secukupnya. Mohon dicatat bahwa sebagian atau seluruh data mungkin diperlukan untuk memastikan situs web berfungsi dengan baik, dan kami juga mungkin diwajibkan oleh hukum yang berlaku untuk menyimpan data tertentu. Kami mungkin akan meyimpan Data Pribadi anda untuk jangka waktu yang lebih lama, tergantung kepada kepentingnan atau persetujuan anda.</p>
-            <p>Kami memiliki manajemen pencatatan yang spesifik dan kebijakan pemyimpanan data dan prosedur, sehingga Data Pribadi anda akan dihapus setelah jangka waktu yang cukup terlewati sesuai dengan ketentuan sebagai berikut:</p>
-            <ol>
-              <li>Kami menyimpan Data Pribadi anda selama anda memiliki hubungan yang berjalan dengan kami (terutama jika anda memiliki akun pada situs web kami).</li>
-              <li>Kami hanya akan memyimpan Data Pribadi anda selama akun anda aktif atau selama diperlukan untuk melaksanakan layanan jasa kepada Anda.</li>
-              <li>Kami menyimpan Data Pribadi anda selama diperlukan guna mematuhi kewajiban hukum dan kontraktual kami secara global.</li>
-              <li>Kami menghapus atau menghilangkan nama pda Data Pribadi anda Ketika sudah tidak lagi diperlukan untuk tujuan-tujuan sebagaimana dimaksudkan pada saat pengumpulan dan pemrosesan data.</li>
-            </ol>
-            <p>
-              <strong>Hak Anda selaku Subjek Data</strong>
-              <br />Anda berhak, dengan ketentuan dan dalam kondisi serta dengan tetap memperhatikan pengecualian-pengecualian yang di sebutkan dalam peraturan hukum, untuk:
-            </p>
-            <ol>
-              <li>Meminta akses terhadap Data Pribadi anda yang kami proses. Hak ini memungkinkan anda utnuk mengetahui apakah kami menyimpan Data Pribadi anda, dan jika demikian, memperoleh informasi dan salinan dari Data Pribadi.</li>
-              <li>Meminta untuk mengoreksi Data Pribadi anda. Hak ini memungkinkan anda untuk mengoreksi Data Pribadi anda jika terdapat ketidakakuratan atau kekurangan.</li>
-              <li>Keberatan terhadap pemrosesan Data Pribadi. Hak ini memungkinan anda untuk meminta kepada kami untuk tidak lagi memproses/mempergunakan Data Pribadi anda</li>
-              <li>Meminta untuk menghapus Data Pribadi. Hal ini memungkinkan anda untuk meminta kami menghapus Data Pribadi anda, termasuk Ketika Data Pribadi tersebut tidak lagi diperlukan</li>
-            </ol>
-            <p>Harap dicatat bahwa sebagian besar Data Pribadi yang kami simpan mengenai anda berasal dari informasi yang anda sampaikan kepada kami, oleh karenanya harap anda dapat menjaga kebenaran dan informasi dengan cara melakukan pembaharuan profil/informasi anda pada situs web kami.</p>
-            <p>Walaupun demikian, harap dicatatat bahwa beberapada Data Pribadi anda mungkin akan dikecualikan dari hak-hak yang disebutkan di atas sesuai dengan peraturan Perlindungan Data Pribadi atau peraturan perundang-undangan lain yang berlaku.</p>
-            <p>Kami telah mengimplementasikan tindakan-tindakan keamanan baik secara organisasi maupun teknis yang sesuai guna melindungi Data Pribadi anda terhadap ancaman proses yang tidak sah atau melawan hukum dan/atau terhadap kerugian yang tidak disengaja, perubahan perubahan, pengungkapan atau akses, atau pembongkaran yang tidak disengaja atau melanggar hukum atau kerusakan. Kami menjaga secara terorganisir, baik secara fisik maupun teknis atas segala pengaturan keamanan untuk seluruh Data Pribadi yang kami Kelola. Kami memiliki protokol, kendali dan kebijakan, prosedur, dan arahan yang relevan guna menjaga pengaturan ini dengan tetap memperhatikan risiko yang berkaitan dengan kategori Data Pribadi dan pemrosesan yang kami lakukan. Kami menyimpan Data Pribadi pada server dengan akses yang terbatas dan berlokasi di fasilitas yang aman, dan prosedur keamanan kami selalu dievaluasi secara berkala. Server kami dilindungi dengan perangkat lunak anti-virus dan firewall, selain juga perangkat keamanan yang lain nya.</p>
-            <p>
-              <strong>Informasi Lebih Lanjut dan Kontak Kami:</strong>
-              <br />&nbsp;
-            </p>
-            <table border="0">
-            <tbody>
-                <tr>
-                <td>Telepon</td>
-                <td>:</td>
-                <td>+62216505668 (hunting)</td>
-                </tr>
-                <tr>
-                <td>Alamat Korespondensi</td>
-                <td>:</td>
-                <td>Jl. Danau Sunter Barat Blkck AIII No. 38-39, North Jakarta 14350</td>
-                </tr>
-                <tr>
-                <td>Email</td>
-                <td>:</td>
-                <td>id.legal@haier.co.id</td>
-                </tr>
-            </tbody>
-            </table>
+             <div dangerouslySetInnerHTML={{ __html: privacyPoicy }}></div>
             </div>
           </div>
         </div>
@@ -112,4 +36,9 @@ function ModalPrivacy() {
     )
 }
 
-export default ModalPrivacy
+const mapStateToProps = (state) => {
+  return {
+      base_url: state.BASE_URL
+  }
+}
+export default connect(mapStateToProps) (ModalPrivacy)
