5cf5bde58d650c33d0877ded834d639875d45f74
syaiful <syaiful.dewarangga@gmail.com>
2021-12-17 16:14:54 +0700

    test

--- src/component/product/userRegisterProduct/UserRegisterProduct.jsx
+++ src/component/product/userRegisterProduct/UserRegisterProduct.jsx
@@ -1,4 +1,16 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import './UserRegisterProduct.css';
+import { connect } from 'react-redux';
+import { useParams, useHistory } from 'react-router-dom';
+import axios from 'axios';
+import SelectSearch, { fuzzySearch } from 'react-select-search';
+import './SelectSearch.css';
+import { format } from 'date-fns';
+import AlertModal from '../../alertModal/AlertModal';
+import { Modal } from 'bootstrap';
+import { client_id, client_secret, grant_type } from '../../../variable';
+import { useTranslation } from 'react-i18next';
+var X2JS = require('x2js');
 
 function UserRegisterProduct() {
   return (
