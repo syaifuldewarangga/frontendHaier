7c60b828c430c06c2b4c673269f45f03118006cd
syaiful <syaiful.dewarangga@gmail.com>
2021-12-22 15:25:04 +0700

    reux

--- src/variable/reduxVariableDevelopment.jsx
+++ src/variable/reduxVariableDevelopment.jsx
@@ -2,7 +2,7 @@ const client_id = 'LKG44Gpar2'
 const client_secret = 'Sn93uLgBwP'
 const grant_type = 'client_credentials'
 
-const BASE_URL = 'http://hsiew.haier.net:9002/api/'
+const BASE_URL = 'https://hsiew.haier.net:9002/api/'
 const URL = 'https://images.e-warranty.click/'
 const GTM_URL = 'https://openplat-sg-aws-test.haier.net/api/'
 const GTM_TOKEN_URL = 'https://openplat-sg-aws-test.haier.net/GTM3HSI/gateway/auth/oauth2/token'
