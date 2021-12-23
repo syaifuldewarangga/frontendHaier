56d236afeed3e10d15e30a47940cc09d9c022576
ahmadsyaifulakbar24 <ipulbelcram@gmail.com>
2021-12-23 09:42:07 +0700

    add new hhtps

--- src/variable/reduxVariableProduction.jsx
+++ src/variable/reduxVariableProduction.jsx
@@ -3,7 +3,7 @@ const client_secret = 'Sn93uLgBwP'
 const grant_type = 'client_credentials'
 
 const BASE_URL = 'https://hsiew.haier.net:9002/api/'
-const URL = 'http://images.aquajapanid.com/'
+const URL = 'https://images.aquajapanid.com/'
 const GTM_URL = 'https://openplat-sg-aws-test.haier.net/api/'
 const GTM_TOKEN_URL = 'https://openplat-sg-aws-test.haier.net/GTM3HSI/gateway/auth/oauth2/token'
 const GSIS_URL = 'https://hsiew.haier.net:9002/oapi/gsis/'
