7e6fd3ff0ff740ec761a7adddbfac8f9113ce463
ahmadsyaifulakbar24 <ipulbelcram@gmail.com>
2021-12-30 09:55:49 +0700

    fixing bug

--- src/variable/reduxVariableDevelopment.jsx
+++ src/variable/reduxVariableDevelopment.jsx
@@ -1,7 +1,7 @@
 const BASE_URL = 'https://ewarranty.aquajapanid.com:9003/api/'
 const URL = 'https://testimages.aquajapanid.com:8180/'
-const GTM_URL = 'https://openplat-sg-aws.haier.net/api/'
-const GTM_TOKEN_URL = 'https://openplat-sg-aws.haier.net/GTM3HSI/gateway/auth/oauth2/token'
+const GTM_URL = 'https://openplat-sg-aws-test.haier.net/api/'
+const GTM_TOKEN_URL = 'https://openplat-sg-aws-test.haier.net/GTM3HSI/gateway/auth/oauth2/token'
 const GSIS_URL = 'https://ewarranty.aquajapanid.com:9003/oapi/gsis/'
 
 export {
