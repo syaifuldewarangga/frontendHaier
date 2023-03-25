cba3c3d07b922a196ee1d972375148fc3577ba8c
faldibule <faldibule@gmail.com>
2023-03-25 12:08:15 +0700

    add port URL image

--- src/variable/reduxVariableDevelopment.jsx
+++ src/variable/reduxVariableDevelopment.jsx
@@ -1,5 +1,5 @@
 const BASE_URL = 'https://testewarranty.aquaelektronik.com:9003/api/'
-const URL = 'https://testimages.aquaelektronik.com/'
+const URL = 'https://testimages.aquaelektronik.com:8180/'
 const GTM_URL = 'https://openplat-sg-aws.haier.net/api/'
 const GTM_TOKEN_URL = 'https://openplat-sg-aws.haier.net/GTM3HSI/gateway/auth/oauth2/token'
 const GSIS_URL = 'https://testewarranty.aquaelektronik.com:9003/oapi/gsis/'
