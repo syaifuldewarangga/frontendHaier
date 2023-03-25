65c56a8ac41e46fcf2c322cdb3631e342f68c687
faldibule <faldibule@gmail.com>
2023-03-25 11:09:10 +0700

    Change domain API

--- src/variable/reduxVariableDevelopment.jsx
+++ src/variable/reduxVariableDevelopment.jsx
@@ -1,9 +1,9 @@
-const BASE_URL = 'https://ewarranty.aquajapanid.com:9003/api/'
-const URL = 'https://testimages.aquajapanid.com:8180/'
+const BASE_URL = 'https://testewarranty.aquaelektronik.com:9003/api/'
+const URL = 'http://testimages.aquaelektronik.com/'
 const GTM_URL = 'https://openplat-sg-aws.haier.net/api/'
 const GTM_TOKEN_URL = 'https://openplat-sg-aws.haier.net/GTM3HSI/gateway/auth/oauth2/token'
-const GSIS_URL = 'https://ewarranty.aquajapanid.com:9003/oapi/gsis/'
-const OAPI_URL = 'https://ewarranty.aquajapanid.com:9003/oapi/'
+const GSIS_URL = 'https://testewarranty.aquaelektronik.com:9003/oapi/gsis/'
+const OAPI_URL = 'https://testewarranty.aquaelektronik.com:9003/oapi/'
 
 export {
     BASE_URL,
@@ -13,4 +13,3 @@ export {
     GSIS_URL,
     OAPI_URL,
 }
-
